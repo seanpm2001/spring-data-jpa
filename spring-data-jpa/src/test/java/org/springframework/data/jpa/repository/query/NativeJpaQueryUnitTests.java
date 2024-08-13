fb876e97e0e614165ed51be79c24621f66af07bd
Mark Paluch <mark.paluch@broadcom.com>
2024-08-13 14:25:08 +0200

    Rewrite string-queries to use constructor expressions when return type is DTO.
    We now rewrite String-based JPA queries to use constructor expressions when either selecting the entity or selecting individual properties.
    
    We do not rewrite queries that already use constructor expressions.

--- spring-data-jpa/src/test/java/org/springframework/data/jpa/repository/query/NativeJpaQueryUnitTests.java
+++ spring-data-jpa/src/test/java/org/springframework/data/jpa/repository/query/NativeJpaQueryUnitTests.java
@@ -66,15 +66,8 @@ class NativeJpaQueryUnitTests {
 	@Test // GH-3546
 	void shouldApplySorting() {
 
-		NativeJpaQuery query = getQuery(TestRepo.class, "find", Sort.class);
-		String sql = query.getSortedQueryString(Sort.by("foo", "bar"));
-
-		assertThat(sql).isEqualTo("SELECT e FROM Employee e order by e.foo asc, e.bar asc");
-	}
-
-	private NativeJpaQuery getQuery(Class<?> repository, String method, Class<?>... args) {
-		Method respositoryMethod = ReflectionUtils.findMethod(repository, method, args);
-		RepositoryMetadata repositoryMetadata = new DefaultRepositoryMetadata(repository);
+		Method respositoryMethod = ReflectionUtils.findMethod(TestRepo.class, "find", Sort.class);
+		RepositoryMetadata repositoryMetadata = new DefaultRepositoryMetadata(TestRepo.class);
 		SpelAwareProxyProjectionFactory projectionFactory = mock(SpelAwareProxyProjectionFactory.class);
 		QueryExtractor queryExtractor = mock(QueryExtractor.class);
 		JpaQueryMethod queryMethod = new JpaQueryMethod(respositoryMethod, repositoryMetadata, projectionFactory,
@@ -85,7 +78,9 @@ class NativeJpaQueryUnitTests {
 		NativeJpaQuery query = new NativeJpaQuery(queryMethod, em, annotation.value(), annotation.countQuery(),
 				QueryRewriter.IdentityQueryRewriter.INSTANCE, QueryMethodEvaluationContextProvider.DEFAULT,
 				new SpelExpressionParser());
-		return query;
+		String sql = query.getSortedQueryString(Sort.by("foo", "bar"), queryMethod.getResultProcessor().getReturnedType());
+
+		assertThat(sql).isEqualTo("SELECT e FROM Employee e order by e.foo asc, e.bar asc");
 	}
 
 	interface TestRepo extends Repository<Object, Object> {
